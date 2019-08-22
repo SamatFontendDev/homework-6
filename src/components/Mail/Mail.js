03b0453a285e9d1439c9a540920725fa90bc89a3
Samatt1991 <samat116rust@yandex.ru>
2019-08-22 17:57:30 +0300

    22.08

--- src/components/Mail/Mail.js
+++ src/components/Mail/Mail.js
@@ -1,2 +1,17 @@
 // Изучите файл `/cypress/integration/homework.spec.js`, чтобы понять,
 // какие классы должен использовать компонент.
+import React from 'react'
+import css from './Mail.module.css'
+
+class Mail extends React.Component{
+    render(){
+        const {data} = this.props;
+        return(
+            <div className={css.container}>
+                {data}
+            </div>
+        )
+    }
+}
+
+export default Mail
